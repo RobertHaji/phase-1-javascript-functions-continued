2802e5c976a3fc575a76707b804a6c6486847c37
Robert Haji <roberthadji66@gmail.com>
2025-03-13 22:00:44 +0300

    adds javascript-function index.js

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
 // code your solution here
+function saturdayFun (venture = 'roller-skate') {
+    return `This Saturday, I want to ${venture}!`;
+}
+function mondayWork(activity = 'go to the office') {
+    return `This Monday, I will ${activity}.`;
+}
+function wrapAdjective(proper = "*") {
+    return function (adjective = 'special') {
+        return `You are ${proper}${adjective}${proper}!`;
+    }
+}
